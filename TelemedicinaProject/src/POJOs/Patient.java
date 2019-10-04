69fcd2210c0d03355b98a9b547f232d575e9d21e
Ines <i.martin59@usp.ceu.es>
2019-10-04 20:07:44 +0200

    ines 04/10 20:07 pojo patient

---
+++ TelemedicinaProject/src/POJOs/Patient.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POJOs;
+
+/**
+ *
+ * @author inesu
+ */
+public class Patient {
+
+    private String nombre;
+    private int edad;
+    private String usuario;
+    private String contraseña;
+    
+    public Patient (String _usuario,String _contraseña, String _nombre, int _edad){
+    usuario = _usuario;
+    contraseña = _contraseña;
+    nombre = _nombre;
+    edad = _edad;
+    
+    }
+         
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Patient{" + "nombre=" + nombre + ", edad=" + edad + ", usuario=" + usuario + ", contrase\u00f1a=" + contraseña + '}';
+    }
+}
